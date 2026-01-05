3e59f6b46024ee111215d1754bf8733b2e3d70a4
Princess Calisaan <m.y.gswaeg@gmail.com>
2026-01-05 13:49:22 +0300

    Add files via upload
    Defined the variables and concatenate.
    Added comments.

---
+++ 01-CodingIsCool.py
@@ -0,0 +1,9 @@
+#exercise 1
+#Define the variables with text
+word_1 = 'Coding'
+word_2 = 'is'
+word_3 = 'Cool'
+
+#use comma or plus to concatenate the variables
+print(word_1, word_2, word_3)
+print(word_1 + ' ' + word_2 + ' ' + word_3)
